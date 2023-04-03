508881e30990352e01a5c6548e49cee18ab80faa
RaphaelDaubelcour <daubelcour63@gmail.com>
2023-04-02 21:09:58 -0400

    Portfolio 1.20

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const projects = document.querySelectorAll('.effect');
 
+    projectsPreview = document.querySelectorAll('.project-preview');
+
+    const navButtons = document.querySelectorAll('.nav-selected')
+
     window.addEventListener('scroll', checkProjects);
 
     checkProjects();
@@ -16,4 +20,84 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         })
     }
-});  
+}); 
+
+var lastSelectedNav = null;
+
+function setDefaultNav(navItem) {
+    // Remove nav-selected class from last selected nav item
+    if (lastSelectedNav != null && lastSelectedNav !== navItem) {
+        lastSelectedNav.classList.remove("nav-selected");
+    }
+
+    // Add nav-selected class to default nav item
+    navItem.classList.add("nav-selected");
+    lastSelectedNav = navItem;
+}
+
+function highlightNav(navItem) {
+    // Add nav-selected class to hovered nav item
+    navItem.classList.add("nav-selected");
+
+    // Remove nav-selected class from last selected nav item
+    if (lastSelectedNav != null && lastSelectedNav !== navItem) {
+        lastSelectedNav.classList.remove("nav-selected");
+    }
+}
+
+function unhighlightNav(navItem) {
+    navItem.classList.remove("nav-selected")
+    
+    // Remove nav-selected class from last selected nav item
+    if (lastSelectedNav != null) {
+        lastSelectedNav.classList.add("nav-selected");
+    }
+}
+
+window.addEventListener('scroll', function() {
+    var hiddenElement = document.getElementById('hiddenElement');
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition < 700) {
+        hiddenElement.classList.remove('hidden');
+    } else {
+        hiddenElement.classList.add('hidden');
+    }
+});
+
+var previewIndex = 0;
+
+function handleLeftClick(){
+    updatePreviewIndex(1);
+    projectsPreview[previewIndex].setAttribute('data-status', 'inactive right');
+
+    updatePreviewIndex(-1);
+    projectsPreview[previewIndex].setAttribute('data-status', 'inactive left');
+
+    updatePreviewIndex(-1);
+    projectsPreview[previewIndex].setAttribute('data-status', 'active');
+}
+
+function handleRightClick(){
+    updatePreviewIndex(-1);
+    projectsPreview[previewIndex].setAttribute('data-status', 'inactive left');
+
+    updatePreviewIndex(1);
+    projectsPreview[previewIndex].setAttribute('data-status', 'inactive right');
+
+    updatePreviewIndex(1);
+    projectsPreview[previewIndex].setAttribute('data-status', 'active');
+}
+
+function updatePreviewIndex(value){
+    previewIndex += value;
+
+    if(previewIndex < 0){
+        previewIndex = projectsPreview.length - 1;
+    }
+    else if(previewIndex == projectsPreview.length){
+        previewIndex = 0;
+    }
+
+    console.log(previewIndex)
+}
